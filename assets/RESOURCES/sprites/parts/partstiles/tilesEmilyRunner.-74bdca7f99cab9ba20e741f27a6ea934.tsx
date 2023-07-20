8fa13753b92d3795cf85ee0cf36ca9dc511a3ef4
Vitor <65993379+Vitor1-1Santana@users.noreply.github.com>
2023-07-20 18:08:25 -0300

    Add files via upload

---
+++ assets/RESOURCES/sprites/parts/partstiles/tilesEmilyRunner.-74bdca7f99cab9ba20e741f27a6ea934.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tilesEmilyRunner.png" tilewidth="32" tileheight="32" spacing="4" margin="2" tilecount="40" columns="10" objectalignment="bottom" fillmode="preserve-aspect-fit">
+ <grid orientation="orthogonal" width="32" height="1"/>
+ <image source="tilesEmilyRunner.png" width="360" height="36"/>
+</tileset>
